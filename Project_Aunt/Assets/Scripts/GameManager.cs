694a4ee0fab35d92a784d9e7d8f06beac35e33fa
leehs730 <leehs730@naver.com>
2023-04-04 19:20:59 +0900

    2023_04_04_FinalCommit
    Clear the darkest side of mind

--- Project_Aunt/Assets/Scripts/GameManager.cs
+++ Project_Aunt/Assets/Scripts/GameManager.cs
@@ -46,7 +46,7 @@ public class GameManager : MonoBehaviour
         LoadLevel(1, 1);
     }
 
-    private void LoadLevel(int world, int stage)
+    public void LoadLevel(int world, int stage)
     {
         this.world = world;
         this.stage = stage;
